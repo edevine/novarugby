4e28eba69f5e81efab3493e006891abee0e35d7b
Eric Devine <devineej@gmail.com>
2014-01-30 08:02:02 -0500

    new template structure finalized

---
+++ view/roster_table.php
@@ -0,0 +1,37 @@
+<?
+$roster = $db->prepare("SELECT lastname, firstname, position, height, weight, school FROM roster ORDER BY lastname ASC, firstname ASC");
+$roster->bindColumn(1, $lname);
+$roster->bindColumn(2, $fname);
+$roster->bindColumn(3, $position);
+$roster->bindColumn(4, $height);
+$roster->bindColumn(5, $weight);
+$roster->bindColumn(6, $school);
+$roster->execute();
+?>
+<main>
+    <header>
+        <h1>Roster</h1>
+    </header>
+    <table class="roster">
+        <thead>
+            <tr>
+                <th>Player
+                <th>Position
+                <th>Height
+                <th>Weight
+                <th>School
+            </tr>
+        </thead>
+        <tbody>
+    <? while ($roster->fetch(PDO::FETCH_BOUND)): ?>
+            <tr>
+                <td><?= $lname, ', ', $fname ?>
+                <td><?= $position ?>
+                <td><?= $height ? floor($height/12) . "' " . $height%12 . '"' : null ?>
+                <td><?= $weight ?>
+                <td><?= $school ?>
+            </tr>
+    <? endwhile ?>
+        </tbody>
+    </table>
+</main>
---
+++ view/venue.php
@@ -0,0 +1,29 @@
+<section class="left">
+    <header>
+        <h1>Venue</h1>
+    </header>
+    <section class="venue">
+        <header>
+            <p class="venue-type">Match Field
+            <h1>Gravely Point Park</h1>
+            <p class="venue-address">George Washington Memorial Pkwy, Arlington, VA 22202
+        </header>
+        <iframe class="venue-map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3106.6124048310003!2d-77.0399286!3d38.8642284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b73e0790e3bb%3A0x33de18e76298dfe9!2sGravelly+Point+Park%2C+George+Washington+Memorial+Parkway%2C+Arlington%2C+VA!5e0!3m2!1sen!2sus!4v1390792978691" width="640" height="360" frameborder="0"></iframe>
+    </section>
+    <section class="venue">
+        <header>
+            <p class="venue-type">Practice Field
+            <h1>Oakton High School</h1>
+            <p class="venue-address">2900 Sutton Rd, Vienna, VA 22181
+        </header>
+        <iframe class="venue-map" src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d3106.0666699815292!2d-77.2826181!3d38.876716449999996!3m2!1i1024!2i768!4f13.1!3m2!1m1!1s0x0%3A0xadbf7ab1176ff030!5e0!3m2!1sen!2sus!4v1390793829211" width="640" height="360" frameborder="0"></iframe>
+    </section>
+    <section class="venue">
+        <header>
+            <p class="venue-type">Social Venue
+            <h1>O'Sullivan's Irish Pub</h1>
+            <p class="venue-address">3207 Washington Blvd, Arlington, VA 22201
+        </header>
+        <iframe class="venue-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.6553479877502!2d-77.09723170000001!3d38.88612650000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b683f34b305b%3A0xe2182f794f29b7bb!2sO&#39;Sullivan&#39;s+Irish+Pub%2C+Washington+Boulevard%2C+Arlington%2C+VA!5e0!3m2!1sen!2sus!4v1390794160052" width="640" height="360" frameborder="0"></iframe>
+    </section>
+</section>
