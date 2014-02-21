11a27f2faec2a5050b7f5e17faf11b1350c2cb29
Eric Devine <devineej@gmail.com>
2014-02-21 15:43:21 -0500

    added credit to global footer

--- inc/footer.php
+++ inc/footer.php
@@ -5,6 +5,7 @@
             <p> &copy; Copyright 2014 Northern Virginia Rugby Football Club. All rights reserved.
             <p> NOVA Rugby &bull; PO Box 3596 &bull; Merrifield, VA 22116-3915
             <p> USA Rugby Club ID: 23498
+            <p> Design by <a href="http://devineej.com">Eric Devine</a>
         </footer>
     </body>
 </html>
